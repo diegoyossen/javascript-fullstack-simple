1efb48b54788ff5ec7e97347b8baef39287acb6c
diegoyossen <diegoyossen@live.com>
2022-12-02 14:56:17 -0300

    MongoDB exito

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/javascriptdb',{
+mongoose.connect('mongodb://127.0.0.1:27017/javascriptdb',{
     useNewUrlParser : true
 })
     .then(db => console.log('DB is connected'))
